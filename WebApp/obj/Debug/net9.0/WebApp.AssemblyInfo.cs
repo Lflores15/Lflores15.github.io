e03c720d53e8e3b513a24385573fb07bc295ff34
Louis Flores <65876661+Lflores15@users.noreply.github.com>
2025-02-18 13:32:25 -0600

    New

--- WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6506599cb56bf76a3619d0afa355948547a9e3ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91eb3e032e0ca12af6e6e94916e9e16d221c1b51")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
